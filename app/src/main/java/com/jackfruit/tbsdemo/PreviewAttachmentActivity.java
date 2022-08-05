8c176de7c17b3dd4251c78f10bd3433e21234d80
猿小蔡 <cjf0898@gmail.com>
2022-08-05 23:12:52 +0800

    Update PreviewAttachmentActivity.java

--- app/src/main/java/com/jackfruit/tbsdemo/PreviewAttachmentActivity.java
+++ app/src/main/java/com/jackfruit/tbsdemo/PreviewAttachmentActivity.java
@@ -36,7 +36,7 @@ public class PreviewAttachmentActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_preview_attachment);
     initView();
     String fileUrl = getIntent().getStringExtra("fileUrl");
     mFile = new File(fileUrl);
